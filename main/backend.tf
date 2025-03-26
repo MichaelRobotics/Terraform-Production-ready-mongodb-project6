ad4f73ab45d471b3a29cb8bb6dc8c8abd62f1f15
MichaelRobotics <user@example.com>
2025-03-26 18:42:26 +0100

    Modified backend

--- main/backend.tf
+++ main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region = "us-east-1"
-    bucket = "learning-terraform"
+    bucket = "learning-terraform-13452"
     key    = "backend/nginx-mongodb-cluster.tfstate"
     dynamodb_table = "dynamoDB-state-locking"
   }
